137d61cd3374edc320801de4715d9a345fe4139c
20033824 <20033824@myrp.edu.sg>
2021-06-08 10:03:39 +0800

    Second Commit

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
@@ -17,7 +17,21 @@ public class ArrayActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView1);
 
         //continue view the code in the worksheet
+        String [] array = new String [3];
 
+        array[0] = "apple";
+        array[1] = "banana";
+        array[2] = "cherry";
+
+
+        System.out.println("Size is : " + array.length);
+
+        String msg = "Fruits\n=====\n";
+        for (int i = 0; i < array.length; i ++){
+            msg += array[i] + "\n";
+        }
+
+        tv.setText(msg);
 
 
     }
--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ArrayListActivity extends AppCompatActivity {
 
     TextView tv;
@@ -17,7 +19,24 @@ public class ArrayListActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView2);
 
         //continue view the code in the worksheet
+        ArrayList <String> al = new ArrayList<String>();
 
+        al.add("apple");
+        al.add("cherry");
+        al.add("banana");
+        al.add("durian");
+        al.remove(1);
+        al.set(al.size()-1,"dragon fruit");
+
+
+        System.out.println("Size is : " + al.size());
+
+        String msg = "Fruits\n=====\n";
+        for (int i = 0; i < al.size(); i ++){
+            msg += al.get(i) + "\n";
+        }
+
+        tv.setText(msg);
 
     }
 }
